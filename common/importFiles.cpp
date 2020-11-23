d72e475ad1e65ac6f41ceddd93ddb4180a95ca34
Fabrizio Corda <cordafab@gmail.com>
2020-11-23 19:28:24 +0100

    Skeleton Load Tweaks

--- common/importFiles.cpp
+++ common/importFiles.cpp
@@ -389,22 +389,23 @@ void loadSkeleton(const char                  * filename,
    {
       istringstream iss(line);
 
-      string token;
-      iss >> token;
-      if (token.size() > 1) continue; // vn,fn  .... I don't care
+      std::vector<std::string> lineData{
+         std::istream_iterator<std::string>(iss), {}
+      };
 
-      if (token[0] == 'j')
+      if (lineData[0].compare("j")==0)
       {
-         int i, father;
-         std::string name;
-         double x, y, z, rx, ry, rz;
-         //iss >> i >> name >> father >> x >> y >> z ;//>> rx >> ry >> rz;
-         iss >> i >> name >> father >> rx >> ry >> rz >> x >> y >> z;
-         jointsTranslations.push_back(cg3::Vec3d(x,y,z));
-         jointsRotations.push_back(cg3::Vec3d(rx,ry,rz));
-         //jointsRotations.push_back(cg3::Vec3d(0,0,0));
-         fathers.push_back(father);
-         names.push_back(name);
+         names.push_back(lineData[2]);
+         fathers.push_back(std::stoi(lineData[3]));
+
+         if(lineData.size()==7){
+            jointsTranslations.push_back(cg3::Vec3d(std::stod(lineData[4]),std::stod(lineData[5]),std::stod(lineData[6])));
+            jointsRotations.push_back   (cg3::Vec3d(0.0, 0.0,0.0));
+         } else
+         if(lineData.size()==10){
+            jointsTranslations.push_back(cg3::Vec3d(std::stod(lineData[7]),std::stod(lineData[8]),std::stod(lineData[9])));
+            jointsRotations.push_back   (cg3::Vec3d(std::stod(lineData[4]),std::stod(lineData[5]),std::stod(lineData[6])));
+         }
       }
    }
    file.close();
